67282c511c770442df592f55162c688c3955d811
Javed Shah Afridi <afridijs18@gmail.com>
2018-11-13 05:47:20 +0500

    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+// Importing React And React Dom From Node Modules
+// When There Is No Extensions (.html,.js, .css)React Assumes That Its From The node_modules folder (Dependencies).
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Creating A Function (ES6 Format) And Assigning It To A Constant
+// This Function Will Return A H1 Tag With The Hellow World To Our Page
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () =>{
+
+
+//If We Have Only One Element The Below Code Will Work But If We Have More Than One Elements It Won't Work So We Need To Change It A Little Bit.    
+return <h1> Hello World !!! </h1>
+
+/** 
+return (
+    <div ClassName = "newElement">
+    <h1> Hello Wordld !!! </h1>
+    <div> Welecome To React </div>
+    </div>
+    ) 
+ */   
+}
+
+// Compiling And Rendering The Code And Displaying It In Our Div With The Id Of root In Our Index.html Page In Our Public Folder
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
