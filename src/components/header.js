dd9125eb5b6a121e14d792ee53ec74eb0b430ea2
Javed Shah Afridi <afridijs18@gmail.com>
2018-11-14 06:07:05 +0500

    Removing The Text Line And Adding A User Object And Returning It's Data On Our Main File
    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 // Importing React From react (Node Modules)
 
 import React from "react";
-
+const user = {
+    name: 'Javed',
+    lastName: 'Afridi',
+    age: 33
+}
 const Header = () => {
   return( <div>
-  Hello React!!!!
+            <div>Name:      {user.name}</div>
+            <div>Last Name: {user.lastName}</div>
+            <div>Age:       {user.age}</div>
       </div>
   )
   }
