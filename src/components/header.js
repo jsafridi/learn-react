f8b0286146ede0f6a1d72cc5228099fd7ade10b5
Javed Shah Afridi <afridijs18@gmail.com>
2018-11-14 05:57:30 +0500

    Remove The Date Function And Add A Line Of Text Just For Testing
    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,10 @@
 // Importing React From react (Node Modules)
 
 import React from "react";
-const getYear = () => {
-    const newDate = new Date();
-    return newDate.getFullYear();
-}
+
 const Header = () => {
   return( <div>
-  The Date Is {
-      getYear()};
+  Hello React!!!!
       </div>
   )
   }
