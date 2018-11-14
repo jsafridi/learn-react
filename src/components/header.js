e8b85bec3542aaa9d6cf439004e4da90c1158726
Javed Shah Afridi <afridijs18@gmail.com>
2018-11-14 06:50:48 +0500

    Another Commit Removig The User Object And Adding Just Header For Return
    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,9 @@
 // Importing React From react (Node Modules)
 
 import React from "react";
-const user = {
-    name: 'Javed',
-    lastName: 'Afridi',
-    age: 33
-}
 const Header = () => {
   return( <div>
-            <div>Name:      {user.name}</div>
-            <div>Last Name: {user.lastName}</div>
-            <div>Age:       {user.age}</div>
+        Header
       </div>
   )
   }
