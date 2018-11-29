69d08bcfbe435411b7045ffdf6d57a1a5f2a8205
Javed Shah Afridi <afridijs18@gmail.com>
2018-11-30 04:32:57 +0500

    Create items Variable And Run A Map Loop To Get Data From Prop.news And Display It On Our Page From The db.json File. So Far So Cool.
    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/components/news_list.js
+++ src/components/news_list.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const NewsList = (props) =>{
+    
+// Creating A Variable Called items It Will Loop Through All The Information We Get From props.news.
+
+   const items = props.news.map((item) => {
     return(
-        <div>{props.news}</div>
+        <div>
+     <h3> {item.title}</h3>
+     <div> {item.feed}
+        </div>
+        </div>
+)
+});
+
+
+
+    return(
+
+
+
+
+// Comment Out The Old Div With {props.news} To Create A New Div For Our Map Function (Loop).
+
+
+// <div>{props.news}</div>
+
+
+
+    // Creating A Div With Items For Our Map Function
+
+    <div>
+        {items}
+    </div>
     )
 }
-
 export default NewsList;
