aa3e97f03c14eef8df0d5d1f766838b2d9e2f41e
Javed Shah Afridi <afridijs18@gmail.com>
2018-12-01 04:18:30 +0500

    Introducing A New Dependency Glamor Which Is A Css Dependency And Use For Styling Our App. Installed Through npm. We Add Two Properties Of Css To Style Our App.
    Signed-off-by: Javed Shah Afridi <afridijs18@gmail.com>

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { css } from 'glamor';
 // Creating A Functional Component NewsItem
 
 // const NewsItem = (props) =>{
@@ -19,8 +19,28 @@ import React from 'react';
 
 
 const NewsItem =({item}) => {
+
+ // Adding Css Importing From glamor Which We Installed From npm (npm install glamor --save) 
+  
+ let news_item = css(
+   {
+    padding:'20px',
+    boxSizing:'border-box',
+    borderBottom:'2px solid grey'
+   }
+ )
+
+ let news_background = css(
+   {
+     background:'lightgreen'
+   }
+ )
+
+ // The Above Two Variables news_item And news_background Were Created To Style Our App It Gives A Padding Of 20 Pixels Border-Boxing And Solid Grey Border Line And The Second Variable Gives Our App A Light Green Background. We Just Pass Two Classes Kind Of Things To Our Div, The One We Want To Styalize Like {...news_item} And {...news_background}. This Is Pure ES6 Formate And Doesn't Do Anything With React.
+
+
     return(
-     <div>
+     <div {...news_item} {...news_background}>
       <h3> {item.title}</h3>
       <div> {item.feed}
        </div>
